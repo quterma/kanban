0abf912afd11073bcbf727712ce05a1c8bc62861
Quterma <wolf921@mail.ru>
2020-10-24 18:17:58 +0300

    add lodash for subscriber throttle, add localstorage subscribe

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
+import throttle from "lodash/throttle";
 import kanbanReducer from "../features/kanban/kanbanSlice";
+import { loadState, saveState } from "../features/kanban/localStorage";
 
-export default configureStore({
+// get state from localStorage
+const persistedState = loadState();
+
+export const store = configureStore({
 	reducer: {
 		kanban: kanbanReducer,
 	},
+	preloadedState: persistedState,
 });
+
+// subscribes for setting state into localStorage
+store.subscribe(
+	throttle(() => {
+		saveState(store.getState());
+	}),
+	1000
+);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import store from "./app/store";
+import { store } from "./app/store";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import "@atlaskit/css-reset";
