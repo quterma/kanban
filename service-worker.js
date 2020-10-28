f9e9d99863e3718cbcc3c5fe383ee7c9c6a9a297
Quterma <wolf921@mail.ru>
2020-10-28 21:17:44 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kanban/precache-manifest.1dd7b637ae62b44a035fa5faa896292d.js"
+  "/kanban/precache-manifest.7889b543260462c5c70e06960ba496ec.js"
 );
 
 self.addEventListener('message', (event) => {
