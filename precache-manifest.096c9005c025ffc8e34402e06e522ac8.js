aa504ac6af380713bf6c35c4458309d047d022c4
Quterma <wolf921@mail.ru>
2020-10-28 18:22:49 +0300

    Updates

---
+++ precache-manifest.096c9005c025ffc8e34402e06e522ac8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1feaf57d2a7e9ff9a93c643918ff3c26",
+    "url": "/kanban/index.html"
+  },
+  {
+    "revision": "5cdd33477fc1877e55ef",
+    "url": "/kanban/static/css/2.3107b260.chunk.css"
+  },
+  {
+    "revision": "5cdd33477fc1877e55ef",
+    "url": "/kanban/static/js/2.a5ac428d.chunk.js"
+  },
+  {
+    "revision": "1af2e467dd1dd801ef7041afe25404ab",
+    "url": "/kanban/static/js/2.a5ac428d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "132bc260af42e875959d",
+    "url": "/kanban/static/js/main.1ce8f995.chunk.js"
+  },
+  {
+    "revision": "f5aa037a105c127e43db",
+    "url": "/kanban/static/js/runtime-main.72bc9640.js"
+  },
+  {
+    "revision": "82509b24f209afe76d0488cdcb0f16b6",
+    "url": "/kanban/static/media/arrow.82509b24.svg"
+  },
+  {
+    "revision": "9eadc42b4d412fc0867f9360c8aa6b49",
+    "url": "/kanban/static/media/ava.9eadc42b.svg"
+  }
+]);
